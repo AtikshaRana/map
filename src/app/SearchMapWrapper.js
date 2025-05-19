b1faf214ed2a6762e67bdf6609a9077b6349d774
Atiksha <atiksha@cleardigital.com>
2025-05-19 16:37:06 +0530

    Fixed the build

--- src/app/SearchMapWrapper.js
+++ src/app/SearchMapWrapper.js
@@ -1,20 +1,14 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import React, { useState } from 'react';
-// import SearchBox from './SearchBox';
-
-// 👇 Dynamically import the map with SSR disabled
+import React from 'react';
 const Maps = dynamic(() => import('./Maps'), { ssr: false });
 
 export default function SearchMapWrapper() {
-  const [position, setPosition] = useState([29.7604, -95.3698]); // Houston
-  const [activeLocation, setActiveLocation] = useState(null);
 
   return (
     <div>
-      {/* <SearchBox setPosition={setPosition} setActiveLocation={setActiveLocation} /> */}
-      <Maps position={position} activeLocation={activeLocation} />
+      <Maps />
     </div>
   );
 }
